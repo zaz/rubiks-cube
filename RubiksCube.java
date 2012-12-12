42f3f45d892519f5bc83dba4978434e86d192e34
Josh Brown <joshbrown15@gmail.com>
2012-12-12 21:08:12 +0000

    Merge face() and twist() functions.
    Allow cube to be manipulated with just one function, albeit with many
    (perhaps too many) arguments.

--- RubiksCube.java
+++ RubiksCube.java
@@ -49,7 +49,11 @@ class Cube {
 		"           "+ bottom[6] +" "+ bottom[7] +" "+ bottom[8] +"\n\n");
 	}
 
-	void face(int[] face, int amount) {
+	void twist(int[] face, int amount,
+	             int[] face0, int[] face1, int[] face2, int[] face3,
+	               int s00, int s01, int s02,   int s10, int s11, int s12,
+	                 int s20, int s21, int s22,   int s30, int s31, int s32) {
+
 		int[] twist = { face[2], face[1], face[0], face[3],
 		                face[6], face[7], face[8], face[5] };
 		face[2] = twist[ (2*amount   ) % 8 ];
@@ -60,57 +64,53 @@ class Cube {
 		face[7] = twist[ (2*amount +5) % 8 ];
 		face[8] = twist[ (2*amount +6) % 8 ];
 		face[5] = twist[ (2*amount +7) % 8 ];
-	}
 
-	void twist(int amount, int[] face0, int[] face1, int[] face2, int[] face3,
-	                    int s00, int s01, int s02, int s10, int s11, int s12,
-	                    int s20, int s21, int s22, int s30, int s31, int s32) {
-		int[] twist = { face0[s00], face0[s01], face0[s02],
+		int[] edges = { face0[s00], face0[s01], face0[s02],
 		                face1[s10], face1[s11], face1[s12],
 		                face2[s20], face2[s21], face2[s22],
 		                face3[s30], face3[s31], face3[s32] };
-		face0[s00] = twist[ (3*amount    ) % 12 ];
-		face0[s01] = twist[ (3*amount  +1) % 12 ];
-		face0[s02] = twist[ (3*amount  +2) % 12 ];
-		face1[s10] = twist[ (3*amount  +3) % 12 ];
-		face1[s11] = twist[ (3*amount  +4) % 12 ];
-		face1[s12] = twist[ (3*amount  +5) % 12 ];
-		face2[s20] = twist[ (3*amount  +6) % 12 ];
-		face2[s21] = twist[ (3*amount  +7) % 12 ];
-		face2[s22] = twist[ (3*amount  +8) % 12 ];
-		face3[s30] = twist[ (3*amount  +9) % 12 ];
-		face3[s31] = twist[ (3*amount +10) % 12 ];
-		face3[s32] = twist[ (3*amount +11) % 12 ];
+		face0[s00] = edges[ (3*amount    ) % 12 ];
+		face0[s01] = edges[ (3*amount  +1) % 12 ];
+		face0[s02] = edges[ (3*amount  +2) % 12 ];
+		face1[s10] = edges[ (3*amount  +3) % 12 ];
+		face1[s11] = edges[ (3*amount  +4) % 12 ];
+		face1[s12] = edges[ (3*amount  +5) % 12 ];
+		face2[s20] = edges[ (3*amount  +6) % 12 ];
+		face2[s21] = edges[ (3*amount  +7) % 12 ];
+		face2[s22] = edges[ (3*amount  +8) % 12 ];
+		face3[s30] = edges[ (3*amount  +9) % 12 ];
+		face3[s31] = edges[ (3*amount +10) % 12 ];
+		face3[s32] = edges[ (3*amount +11) % 12 ];
 	}
 
 	void right(int amount) {
-		face(right, amount);
-		twist(amount, front, bottom, back, top, 2,5,8, 2,5,8, 6,3,0, 2,5,8);
+		twist(  right, amount,  front, bottom,   back,    top,
+		                        2,5,8,  2,5,8,  6,3,0,  2,5,8 );
 	}
 
 	void left(int amount) {
-		face(left, amount);
-		twist(amount, front, top, back, bottom, 6,3,0, 6,3,0, 2,5,8, 6,3,0);
+		twist(   left, amount,  front,    top,   back, bottom,
+		                        6,3,0,  6,3,0,  2,5,8,  6,3,0 );
 	}
 
 	void top(int amount) {
-		face(top, amount);
-		twist(amount, front, right, back, left, 0,1,2, 0,1,2, 0,1,2, 0,1,2);
+		twist(    top, amount,  front,  right,   back,   left,
+		                        0,1,2,  0,1,2,  0,1,2,  0,1,2 );
 	}
 
 	void bottom(int amount) {
-		face(bottom, amount);
-		twist(amount, front, left, back, right, 8,7,6, 8,7,6, 8,7,6, 8,7,6);
+		twist( bottom, amount,  front,   left,   back,  right,
+		                        8,7,6,  8,7,6,  8,7,6,  8,7,6 );
 	}
 
 	void front(int amount) {
-		face(front, amount);
-		twist(amount, top, left, bottom, right, 8,7,6, 2,5,8, 0,1,2, 6,3,0);
+		twist(  front, amount,    top,   left, bottom,  right,
+		                        8,7,6,  2,5,8,  0,1,2,  6,3,0 );
 	}
 
 	void back(int amount) {
-		face(back, amount);
-		twist(amount, top, right, bottom, left, 0,1,2, 2,5,8, 8,7,6, 6,3,0);
+		twist(   back, amount,    top,  right, bottom,   left,
+		                        0,1,2,  2,5,8,  8,7,6,  6,3,0 );
 	}
 
 	// Overload functions to make clockwise rotation default:
